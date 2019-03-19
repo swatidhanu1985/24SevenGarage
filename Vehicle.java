edde043c730f5cf22cde63e8108da4aa790e24bd
swatidhanu1985 <45289147+swatidhanu1985@users.noreply.github.com>
2019-03-19 16:45:37 +0100

    Update Vehicle.java

--- Vehicle.java
+++ Vehicle.java
@@ -1,28 +1,30 @@
 import java.util.Date;
 
 public class Vehicle {
-	
+
 	String vehicleNumber;
 	Date inTime;
-	
-	public Vehicle (String vehicleNumber, Date inTime) {
+
+	public Vehicle(String vehicleNumber, Date inTime) {
 		this.vehicleNumber = vehicleNumber;
 		this.inTime = inTime;
-		
+
 	}
+
 	public String getVehicleNumber() {
 		return vehicleNumber;
 	}
+
 	public void setVehicleNumber(String vehicleNumber) {
 		this.vehicleNumber = vehicleNumber;
 	}
+
 	public Date getInTime() {
 		return inTime;
 	}
+
 	public void setInTime(Date inTime) {
 		this.inTime = inTime;
 	}
-	
-	
 
 }
